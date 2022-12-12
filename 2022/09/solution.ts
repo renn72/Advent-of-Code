82fa10af3335bcbee53e38a9bf44b6ca1e3de09a
renn72 <simplesam8856@gmail.com>
2022-12-12 11:07:18 +1100

    2022 09

---
+++ 2022/09/solution.ts
@@ -0,0 +1,389 @@
+/* From https://adventofcode.com/2022/day/9 
+ Day 9: Rope Bridge 
+This rope bridge creaks as you walk along it. You aren't sure how old it is, or whether it can even support your weight.
+It seems to support the Elves just fine, though. The bridge spans a gorge which was carved out by the massive river far below you.
+You step carefully; as you do, the ropes stretch and twist. You decide to distract yourself by modeling rope physics; maybe you can even figure out where not to step.
+Consider a rope with a knot at each end; these knots mark the head and the tail of the rope. If the head moves far enough away from the tail, the tail is pulled toward the head.
+Due to nebulous reasoning involving Planck lengths, you should be able to model the positions of the knots on a two-dimensional grid. Then, by following a hypothetical series of motions (your puzzle input) for the head, you can determine how the tail will move.
+Due to the aforementioned Planck lengths, the rope must be quite short; in fact, the head (H) and tail (T) must always be touching (diagonally adjacent and even overlapping both count as touching):
+....
+.TH.
+....
+
+....
+.H..
+..T.
+....
+
+...
+.H. (H covers T)
+...
+
+If the head is ever two steps directly up, down, left, or right from the tail, the tail must also move one step in that direction so it remains close enough:
+.....    .....    .....
+.TH.. -> .T.H. -> ..TH.
+.....    .....    .....
+
+...    ...    ...
+.T.    .T.    ...
+.H. -> ... -> .T.
+...    .H.    .H.
+...    ...    ...
+
+Otherwise, if the head and tail aren't touching and aren't in the same row or column, the tail always moves one step diagonally to keep up:
+.....    .....    .....
+.....    ..H..    ..H..
+..H.. -> ..... -> ..T..
+.T...    .T...    .....
+.....    .....    .....
+
+.....    .....    .....
+.....    .....    .....
+..H.. -> ...H. -> ..TH.
+.T...    .T...    .....
+.....    .....    .....
+
+You just need to work out where the tail goes as the head follows a series of motions. Assume the head and the tail both start at the same position, overlapping.
+For example:
+R 4
+U 4
+L 3
+D 1
+R 4
+D 1
+L 5
+R 2
+
+This series of motions moves the head right four steps, then up four steps, then left three steps, then down one step, and so on. After each step, you'll need to update the position of the tail if the step means the head is no longer adjacent to the tail. Visually, these motions occur as follows (s marks the starting position as a reference point):
+== Initial State ==
+
+......
+......
+......
+......
+H.....  (H covers T, s)
+
+== R 4 ==
+
+......
+......
+......
+......
+TH....  (T covers s)
+
+......
+......
+......
+......
+sTH...
+
+......
+......
+......
+......
+s.TH..
+
+......
+......
+......
+......
+s..TH.
+
+== U 4 ==
+
+......
+......
+......
+....H.
+s..T..
+
+......
+......
+....H.
+....T.
+s.....
+
+......
+....H.
+....T.
+......
+s.....
+
+....H.
+....T.
+......
+......
+s.....
+
+== L 3 ==
+
+...H..
+....T.
+......
+......
+s.....
+
+..HT..
+......
+......
+......
+s.....
+
+.HT...
+......
+......
+......
+s.....
+
+== D 1 ==
+
+..T...
+.H....
+......
+......
+s.....
+
+== R 4 ==
+
+..T...
+..H...
+......
+......
+s.....
+
+..T...
+...H..
+......
+......
+s.....
+
+......
+...TH.
+......
+......
+s.....
+
+......
+....TH
+......
+......
+s.....
+
+== D 1 ==
+
+......
+....T.
+.....H
+......
+s.....
+
+== L 5 ==
+
+......
+....T.
+....H.
+......
+s.....
+
+......
+....T.
+...H..
+......
+s.....
+
+......
+......
+..HT..
+......
+s.....
+
+......
+......
+.HT...
+......
+s.....
+
+......
+......
+HT....
+......
+s.....
+
+== R 2 ==
+
+......
+......
+.H....  (H covers T)
+......
+s.....
+
+......
+......
+.TH...
+......
+s.....
+
+After simulating the rope, you can count up all of the positions the tail visited at least once. In this diagram, s again marks the starting position (which the tail also visited) and # marks other positions the tail visited:
+..##..
+...##.
+.####.
+....#.
+s###..
+
+So, there are 13 positions the tail visited at least once.
+Simulate your complete hypothetical series of motions. How many positions does the tail of the rope visit at least once?
+
+*/
+// @ts-ignore
+import { intval, log, logList } from "../../tools.ts";
+// @ts-ignore
+import { puzzle } from "../../puzzle.ts";
+// @ts-ignore
+import type { Puzzle } from "../../puzzle.ts"
+
+const findAnswers = (entries: string[][], isLog = true) => {
+  const answers = {
+      a: 0,
+      b: 0,
+    };
+  // Part One
+  const directions : string[] = []
+  for(const e of entries){
+    for(let i=0; i<intval(e[1]); i++){
+      directions.push(e[0])
+    }
+  }
+
+
+  let x = 5555
+  let y = 5555
+  let loc = x.toString() + y.toString()
+  const visited = new Set()
+  visited.add(loc)
+
+  const isTouching = (x, y, loc) => {
+    let t = false
+    const tailX = intval(loc.slice(0,4))
+    const tailY = intval(loc.slice(4))
+    if ((x == tailX && y == tailY) ||
+        (x == tailX + 1 && y == tailY) ||
+        (x == tailX - 1 && y == tailY) ||
+        (x == tailX + 1 && y == tailY + 1) ||
+        (x == tailX + 1 && y == tailY - 1) ||
+        (x == tailX - 1 && y == tailY + 1) ||
+        (x == tailX - 1 && y == tailY - 1) ||
+        (x == tailX && y == tailY - 1) ||
+        (x == tailX && y == tailY + 1)
+    ) t = true
+
+    return t
+  }
+
+  for(const d of directions){
+    const oldX = x
+    const oldY = y
+    if(d === 'U') y++
+    if(d === 'D') y--
+    if(d === 'L') x--
+    if(d === 'R') x++
+    if (!isTouching(x, y, loc)) {
+      loc = oldX.toString() + oldY.toString() 
+    }
+    visited.add(loc)
+  }
+
+
+  answers.a = visited.size
+
+  // Part Two
+  
+  const rope = [
+    {x: 0, y: 0},
+    {x: 0, y: 0},
+    {x: 0, y: 0},
+    {x: 0, y: 0},
+    {x: 0, y: 0},
+    {x: 0, y: 0},
+    {x: 0, y: 0},
+    {x: 0, y: 0},
+    {x: 0, y: 0},
+    {x: 0, y: 0},
+  ]
+
+  visited.clear()
+
+  for(const dir of directions){
+    const newHead = {...rope[0]}
+
+    if(dir === 'U') newHead.y++
+    if(dir === 'D') newHead.y--
+    if(dir === 'L') newHead.x--
+    if(dir === 'R') newHead.x++
+
+    rope[0] = newHead
+
+    for (let knot = 0; knot < rope.length - 1; knot++) {
+      let dx = rope[knot].x - rope[knot + 1].x;
+      let dy = rope[knot].y - rope[knot + 1].y;
+      if (Math.abs(dx) > 1) {
+        rope[knot + 1].x += dx > 0 ? 1 : -1;
+        if (Math.abs(dy) != 0) rope[knot + 1].y += dy > 0 ? 1 : -1;
+      } else if (Math.abs(dy) > 1) {
+        rope[knot + 1].y += dy > 0 ? 1 : -1;
+        if (Math.abs(dx) != 0) rope[knot + 1].x += dx > 0 ? 1 : -1;
+      }
+    }
+
+    visited.add(`${rope[9].x},${rope[9].y}`)
+
+  }
+  answers.b = visited.size
+
+  if (isLog) {
+    log(entries);
+    log(directions);
+    log(answers);
+  }
+  return answers;
+};
+const testPart1 = async (input: string): Promise<boolean> => {
+  const puzzle_input = await puzzle.parseInput(input);
+  const answers = findAnswers(puzzle_input.blocks[0]);
+
+  return answers.a == 13 ? true : false;
+};
+const solvePart1 = async (): Promise<number> => {
+  const puzzle_input = await puzzle.parseInput();
+  const answers = findAnswers(puzzle_input.blocks[0], false);
+
+  return answers.a;
+};
+const testPart2 = async (input: string): Promise<boolean> => {
+  const puzzle_input = await puzzle.parseInput(input);
+  const answers = findAnswers(puzzle_input.blocks[0]);
+
+  return answers.b == 9 ? true : false;
+};
+const solvePart2 = async (): Promise<number> => {
+  const puzzle_input = await puzzle.parseInput();
+  const answers = findAnswers(puzzle_input.blocks[0], false);
+
+  return answers.b;
+};
+const test_input = `
+R 4
+U 4
+L 3
+D 1
+R 4
+D 1
+L 5
+R 2
+`;
+
+// const part1_correct = await testPart1(test_input);
+// const part1 = await solvePart1();
+// log("    part 1: ", part1, part1_correct);
+const part2_correct = await testPart2(test_input);
+const part2 = await solvePart2();
+log("    part 2: ", part2, part2_correct);
