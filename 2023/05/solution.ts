8ecaede70727324530d4ed639489db9b0e50f4af
renn72 <simplesam8856@gmail.com>
2023-12-18 14:20:55 +1100

    day 5 part 1

--- 2023/05/solution.ts
+++ 2023/05/solution.ts
@@ -83,62 +83,309 @@ Seed 13, soil 13, fertilizer 52, water 41, light 34, temperature 34, humidity 35
 So, the lowest location number in this example is 35.
 What is the lowest location number that corresponds to any of the initial seed numbers?
 
+Your puzzle answer was 382895070.
+
+
+
+The first half of this puzzle is complete! It provides one gold star: *
+
+--- Part Two ---
+Everyone will starve if you only plant such a small number of seeds. Re-reading the almanac, it looks like the seeds: line actually describes ranges of seed numbers.
+
+The values on the initial seeds: line come in pairs. Within each pair, the first value is the start of the range and the second value is the length of the range. So, in the first line of the example above:
+
+seeds: 79 14 55 13
+This line describes two ranges of seed numbers to be planted in the garden. The first range starts with seed number 79 and contains 14 values: 79, 80, ..., 91, 92. The second range starts with seed number 55 and contains 13 values: 55, 56, ..., 66, 67.
+
+Now, rather than considering four seed numbers, you need to consider a total of 27 seed numbers.
+
+In the above example, the lowest location number can be obtained from seed number 82, which corresponds to soil 84, fertilizer 84, water 84, light 77, temperature 45, humidity 46, and location 46. So, the lowest location number is 46.
+
+Consider all of the initial seed numbers listed in the ranges on the first line of the almanac. What is the lowest location number that corresponds to any of the initial seed numbers?
+
 */
 // @ts-ignore
 import { intval, log, logList } from "../../tools.ts";
 // @ts-ignore
 import { puzzle } from "../../puzzle.ts";
 // @ts-ignore
-import type { Puzzle } from "../../puzzle.ts"
+import type { Puzzle } from "../../puzzle.ts";
 
-const findAnswers = (entries: string[][], isLog = true) => {
+const findAnswers = (entries: string[][][], isLog = true) => {
   const answers = {
-      a: 0,
-      b: 0,
-    };
+    a: 0,
+    b: 0,
+  };
   // Part One
 
+  const input = {
+    seeds: [...entries[0][0].slice(1).map(intval)] as number[],
+    seedToSoil: [
+      ...entries[1].slice(1).map((e) => e.map(intval)),
+    ] as number[][],
+    soilToFertilizer: [
+      ...entries[2].slice(1).map((e) => e.map(intval)),
+    ] as number[][],
+    fertilizerToWater: [
+      ...entries[3].slice(1).map((e) => e.map(intval)),
+    ] as number[][],
+    waterToLight: [
+      ...entries[4].slice(1).map((e) => e.map(intval)),
+    ] as number[][],
+    lightToTemperature: [
+      ...entries[5].slice(1).map((e) => e.map(intval)),
+    ] as number[][],
+    temperatureToHumidity: [
+      ...entries[6].slice(1).map((e) => e.map(intval)),
+    ] as number[][],
+    humidityToLocation: [
+      ...entries[7].slice(1).map((e) => e.map(intval)),
+    ] as number[][],
+  };
 
+  const loc = [] as number[];
+
+  for (let i = 0; i < input.seeds.length; i++) {
+    break;
+    let seed = input.seeds[i];
+
+    // seed to soil
+    let soil = input.seedToSoil.find(
+      (s) => s[1] <= seed && seed <= s[1] + s[2] - 1
+    );
+    let seedToSoil = seed;
+    if (soil) {
+      seedToSoil = soil[0] + seed - soil[1];
+    }
+    if (isLog && seed == 79) log("seed to soil", seedToSoil);
+
+    // soil to fertilizer
+    let fertilizer = input.soilToFertilizer.find(
+      (s) => s[1] <= seedToSoil && seedToSoil <= s[1] + s[2] - 1
+    );
+    let soilToFertilizer = seedToSoil;
+    if (fertilizer) {
+      soilToFertilizer = fertilizer[0] + seedToSoil - fertilizer[1];
+    }
+    if (isLog && seed == 79) log("soil to fertilizer", soilToFertilizer);
+
+    // fertilizer to water
+    let water = input.fertilizerToWater.find(
+      (s) => s[1] <= soilToFertilizer && soilToFertilizer <= s[1] + s[2] - 1
+    );
+    let fertilizerToWater = soilToFertilizer;
+    if (water) {
+      fertilizerToWater = water[0] + soilToFertilizer - water[1];
+    }
+    if (isLog && seed == 79) log("fertilizer to water", fertilizerToWater);
+
+    // water to light
+    let light = input.waterToLight.find(
+      (s) => s[1] <= fertilizerToWater && fertilizerToWater <= s[1] + s[2] - 1
+    );
+    let waterToLight = fertilizerToWater;
+    if (light) {
+      waterToLight = light[0] + fertilizerToWater - light[1];
+    }
+    if (isLog && seed == 79) log("water to light", waterToLight);
+
+    // light to temperature
+    let temperature = input.lightToTemperature.find(
+      (s) => s[1] <= waterToLight && waterToLight <= s[1] + s[2] - 1
+    );
+    let lightToTemperature = waterToLight;
+    if (temperature) {
+      lightToTemperature = temperature[0] + waterToLight - temperature[1];
+    }
+    if (isLog && seed == 79) log("light to temperature", lightToTemperature);
+
+    // temperature to humidity
+    let humidity = input.temperatureToHumidity.find(
+      (s) => s[1] <= lightToTemperature && lightToTemperature <= s[1] + s[2] - 1
+    );
+    let temperatureToHumidity = lightToTemperature;
+    if (humidity) {
+      temperatureToHumidity = humidity[0] + lightToTemperature - humidity[1];
+    }
+    if (isLog && seed == 79)
+      log("temperature to humidity", temperatureToHumidity);
+
+    // humidity to location
+    let location = input.humidityToLocation.find(
+      (s) =>
+        s[1] <= temperatureToHumidity &&
+        temperatureToHumidity <= s[1] + s[2] - 1
+    );
+    let humidityToLocation = temperatureToHumidity;
+    if (location) {
+      humidityToLocation = location[0] + temperatureToHumidity - location[1];
+    }
+    if (isLog && seed == 79) log("humidity to location", humidityToLocation);
+    loc.push(humidityToLocation);
+  }
+
+  answers.a = Math.min(...loc);
 
   // Part Two
 
+  const calcLoc = (seed: number) => {
+    // seed to soil
+    let soil = input.seedToSoil.find(
+      (s) => s[1] <= seed && seed <= s[1] + s[2] - 1
+    );
+    let seedToSoil = seed;
+    if (soil) {
+      seedToSoil = soil[0] + seed - soil[1];
+    }
+    if (isLog && seed == 79) log("seed to soil", seedToSoil);
+
+    // soil to fertilizer
+    let fertilizer = input.soilToFertilizer.find(
+      (s) => s[1] <= seedToSoil && seedToSoil <= s[1] + s[2] - 1
+    );
+    let soilToFertilizer = seedToSoil;
+    if (fertilizer) {
+      soilToFertilizer = fertilizer[0] + seedToSoil - fertilizer[1];
+    }
+    if (isLog && seed == 79) log("soil to fertilizer", soilToFertilizer);
+
+    // fertilizer to water
+    let water = input.fertilizerToWater.find(
+      (s) => s[1] <= soilToFertilizer && soilToFertilizer <= s[1] + s[2] - 1
+    );
+    let fertilizerToWater = soilToFertilizer;
+    if (water) {
+      fertilizerToWater = water[0] + soilToFertilizer - water[1];
+    }
+    if (isLog && seed == 79) log("fertilizer to water", fertilizerToWater);
+
+    // water to light
+    let light = input.waterToLight.find(
+      (s) => s[1] <= fertilizerToWater && fertilizerToWater <= s[1] + s[2] - 1
+    );
+    let waterToLight = fertilizerToWater;
+    if (light) {
+      waterToLight = light[0] + fertilizerToWater - light[1];
+    }
+    if (isLog && seed == 79) log("water to light", waterToLight);
+
+    // light to temperature
+    let temperature = input.lightToTemperature.find(
+      (s) => s[1] <= waterToLight && waterToLight <= s[1] + s[2] - 1
+    );
+    let lightToTemperature = waterToLight;
+    if (temperature) {
+      lightToTemperature = temperature[0] + waterToLight - temperature[1];
+    }
+    if (isLog && seed == 79) log("light to temperature", lightToTemperature);
+
+    // temperature to humidity
+    let humidity = input.temperatureToHumidity.find(
+      (s) => s[1] <= lightToTemperature && lightToTemperature <= s[1] + s[2] - 1
+    );
+    let temperatureToHumidity = lightToTemperature;
+    if (humidity) {
+      temperatureToHumidity = humidity[0] + lightToTemperature - humidity[1];
+    }
+    if (isLog && seed == 79)
+      log("temperature to humidity", temperatureToHumidity);
+
+    // humidity to location
+    let location = input.humidityToLocation.find(
+      (s) =>
+        s[1] <= temperatureToHumidity &&
+        temperatureToHumidity <= s[1] + s[2] - 1
+    );
+    let humidityToLocation = temperatureToHumidity;
+    if (location) {
+      humidityToLocation = location[0] + temperatureToHumidity - location[1];
+    }
+    if (isLog && seed == 79) log("humidity to location", humidityToLocation);
+    return humidityToLocation;
+  };
+
+  let lowest = 0;
+
+  for (let i = 0; i < input.seeds.length; i += 2) {
+    let seed = input.seeds[i];
+    let length = input.seeds[i + 1];
+    for (let j = 0; j < length; j++) {
+      const loc = calcLoc(seed + j);
+      if (loc < lowest || lowest == 0) {
+        lowest = loc;
+      }
+    }
+  }
+  answers.b = lowest;
 
   if (isLog) {
-    log(entries);
+    log(input);
   }
   return answers;
 };
 const testPart1 = async (input: string): Promise<boolean> => {
   const puzzle_input = await puzzle.parseInput(input);
-  const answers = findAnswers(puzzle_input.blocks[0]);
+  const answers = findAnswers(puzzle_input.blocks);
 
-  return answers.a == 1 ? true : false;
+  return answers.a == 35 ? true : false;
 };
 const solvePart1 = async (): Promise<number> => {
   const puzzle_input = await puzzle.parseInput();
-  const answers = findAnswers(puzzle_input.blocks[0], false);
+  const answers = findAnswers(puzzle_input.blocks, false);
 
   return answers.a;
 };
 const testPart2 = async (input: string): Promise<boolean> => {
   const puzzle_input = await puzzle.parseInput(input);
-  const answers = findAnswers(puzzle_input.blocks[0]);
+  const answers = findAnswers(puzzle_input.blocks);
 
-  return answers.b == 1 ? true : false;
+  return answers.b == 46 ? true : false;
 };
 const solvePart2 = async (): Promise<number> => {
   const puzzle_input = await puzzle.parseInput();
-  const answers = findAnswers(puzzle_input.blocks[0], false);
+  const answers = findAnswers(puzzle_input.blocks, false);
 
   return answers.b;
 };
 const test_input = `
+seeds: 79 14 55 13
 
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4
 `;
 
-const part1_correct = await testPart1(test_input);
-const part1 = await solvePart1();
-log("    part 1: ", part1, part1_correct);
-// const part2_correct = await testPart2(test_input);
-// const part2 = await solvePart2();
-// log("    part 2: ", part2, part2_correct);
+// const part1_correct = await testPart1(test_input);
+// const part1 = await solvePart1();
+// log("    part 1: ", part1, part1_correct);
+const part2_correct = await testPart2(test_input);
+const part2 = await solvePart2();
+log("    part 2: ", part2, part2_correct);
